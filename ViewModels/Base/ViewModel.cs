1303a0cae3d4bc90d088bd5a31afe6fca7b2a485
Александр Каменских <sanyakamen96@mail.ru>
2023-08-24 13:12:14 +0500

    Add project files.

---
+++ ViewModels/Base/ViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_11.ViewModels.Base
+{
+    internal abstract class ViewModel : INotifyPropertyChanged
+    {  
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        /// <summary>
+        /// Вызов события. Если явно не указывается название свойства, 
+        /// то используется имя свойства, в котором происходит вызов
+        /// </summary>
+        /// <param name="propertyName"></param>
+        protected virtual void OnPropertyChanged([CallerMemberName] string? PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        /// <summary>
+        /// Метод для сеттера свойств
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="field">Поле во VM</param>
+        /// <param name="value">Значение, записываемое в поле</param>
+        /// <param name="PropertyName">Название обновляемого свойства 
+        /// (если вызывается в самом свойстве, то можно не указывать)</param>
+        /// <returns>Вызывает событие для изменения интерфейса</returns>
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string? PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+    }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Homework_11.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_11.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+
+
+        /// <summary>
+        /// Конструктор класса (описываются команды)
+        /// </summary>
+        public MainWindowViewModel()
+        {
+            #region Commands
+            //AddCommand = new LambdaCommand(OnAddCommandExecuted, CanAddCommandExecute);
+            #endregion
+        }
+    }
+}
