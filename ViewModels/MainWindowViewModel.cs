d137569ff5a361e248c4bf171299b76c00fa6d3c
Александр Каменских <sanyakamen96@mail.ru>
2023-08-24 13:45:51 +0500

    Создание инфрастуктуры для создания обработчиков кнопок. Проверка на смене заголовка по нажатии кнопки.

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,11 @@
-﻿using Homework_11.ViewModels.Base;
+﻿using Homework_11.Infrastructure.Commands;
+using Homework_11.ViewModels.Base;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Homework_11.ViewModels
 {
@@ -17,6 +19,20 @@ namespace Homework_11.ViewModels
             set => Set(ref _title, value);
         }
 
+        #region AddCommand
+
+        public ICommand AddCommand { get; } //здесь живет сама команда (это по сути обычное свойство, чтобы его можно было вызвать из хамл)
+
+        private void OnAddCommandExecuted(object p) //логика команды
+        {
+            _title = "БлаБлаБла";
+            OnPropertyChanged("Title");
+        }
+
+        private bool CanAddCommandExecute(object p) => true; //если команда должна быть доступна всегда, то просто возвращаем true
+
+        #endregion
+
 
         /// <summary>
         /// Конструктор класса (описываются команды)
@@ -24,7 +40,7 @@ namespace Homework_11.ViewModels
         public MainWindowViewModel()
         {
             #region Commands
-            //AddCommand = new LambdaCommand(OnAddCommandExecuted, CanAddCommandExecute);
+            AddCommand = new LambdaCommand(OnAddCommandExecuted, CanAddCommandExecute);
             #endregion
         }
     }
