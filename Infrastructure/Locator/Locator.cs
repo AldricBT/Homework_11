d8b71481ac2a2fd6a2c8fb4a848881f621da89d0
Александр Каменских <aldricBT@yandex.ru>
2023-08-30 14:34:05 +0500

    добавление локатора и второй VM

---
+++ Infrastructure/Locator/Locator.cs
@@ -0,0 +1,23 @@
+﻿using Homework_11.Models;
+using Homework_11.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_11.Infrastructure.Locator
+{
+    public class Locator
+    {
+        
+        internal MainWindowViewModel MainWindowVM { get; }
+        internal AddClientWindowViewModel AddClientWindowVM { get; }
+
+        public Locator()
+        {
+            MainWindowVM = new MainWindowViewModel();
+            AddClientWindowVM = new AddClientWindowViewModel();
+        }
+    }
+}
---
+++ ViewModels/AddClientWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Homework_11.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_11.ViewModels
+{
+    internal class AddClientWindowViewModel : ViewModel
+    {
+
+    }
+}
