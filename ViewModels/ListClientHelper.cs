b46b7c7aec9a72db9d29ac49c9d1345c3963a4b2
Александр Каменских <aldricBT@yandex.ru>
2023-08-31 11:37:21 +0500

    валидация данных. однако пустое окно остается

--- ViewModels/ListClientHelper.cs
+++ ViewModels/ListClientHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Homework_11.Models;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -6,7 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace Homework_11.Models
+namespace Homework_11.ViewModels
 {
     public class ListClientHelper : DependencyObject
     {
